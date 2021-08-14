8c23795488759351ffd2268da3e4076f36d19786
Ralf Stephan <me@ralf-stephan.name>
2021-08-14 06:26:03 +0000

    Add files for compiling and linking.
    modules.mk:
    
    * add boolean and status_codes

--- src/modules.mk
+++ src/modules.mk
@@ -1,4 +1,14 @@
-mod_ecl.la: mod_ecl.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_ecl.lo
+mod_ecl.la: \
+	mod_ecl.slo \
+	boolean.slo \
+	status_codes.slo
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version \
+	mod_ecl.lo \
+	boolean.lo \
+	status_codes.lo
+
 DISTCLEAN_TARGETS = modules.mk
-shared =  mod_ecl.la
+
+static =
+
+shared = mod_ecl.la
