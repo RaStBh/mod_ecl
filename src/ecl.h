4c749f41f8088527498cfca99a574d872e5c7830
Ralf Stephan <me@ralf-stephan.name>
2021-08-15 08:14:51 +0000

    Add new files.
    ecl.h, ecl.c:
    
    * add new files

---
+++ src/ecl.h
@@ -0,0 +1,38 @@
+// This file is part of RaSt mod_ecl.
+//
+// RaSt mod_ecl is  an extension module for the Apache  HTTP Server.  It enables
+// the Apache HTTP  Server to run scripts written in  the Embeddable Common-Lisp
+// language.
+//
+// Copyright (c) 2021 Ralf Stephan (RaSt) <me@ralf-stephan.name>
+//
+// RaSt mod_ecl is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either  version 3 of the  License, or (at your  option) any later
+// version.
+//
+// RaSt mod_ecl is distributed  in the hope that it will  be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR  A PARTICULAR  PURPOSE.   See the  GNU General  Public  License for  more
+// details.
+//
+// You should have received a copy of  the GNU General Public License along with
+// RaSt     mod_ecl     in     the     file    COPYING.      If     not,     see
+// <https://www.gnu.org/licenses/>.
+
+
+
+/**
+ * @file
+ *
+ * @brief Evaluate Embeddable Common-Lisp scripts.
+ *
+ * @details
+ */
+
+
+
+#ifndef ECL_H
+#define ECL_H
+
+#endif
